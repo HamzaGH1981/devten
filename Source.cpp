93d3a2ac2c02c2cd06a0af2bdae303d06a0c19bb
H1981 <ghomranihamzacne@gmail.com>
2020-03-27 22:02:25 +0100

    add Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void display()
+{
+
+}
+
+void calcule(int start, int end)
+{
+
+}
+
+int main()
+{
+	try
+	{
+		int start, end;
+
+		cout<<"Entrer The Start Number: "<<endl;
+		cin>>start;
+		cout<<"Entrer The End Number: "<<endl;
+		cin>>end;
+
+		calcule(start, end);
+	}
+	catch(exception ex)
+	{
+		cout<<"errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrre";
+  	    cin.get();
+
+	}
+
+	 cout<<"ghomranihamzacne@gmail.com                                       03.2020";
+	 
+    char out[1];
+    cin>>out;
+	cin.get();
+
+}
